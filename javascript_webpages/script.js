326a130e5601f8de8c4e4b7c3101a401109ef5f8
Mahdi-Pranto <mahdiprantoblog@gmail.com>
2024-06-22 23:35:32 +0600

    currency converter done

--- javascript_webpages/script.js
+++ javascript_webpages/script.js
@@ -32,6 +32,39 @@ function displayFortune(array) {
     
 }
 
+
+// for convert currency script 
+const conversionRates = {
+    usd: { usd: 1, gbp: 2.03032, cad: 1.01941, eur: 1.41544, aud: 0.88297, },
+    gbp: { usd: 0.49246, gbp: 1, cad: 0.50221, eur: 0.69714, aud: 0.43497, },
+    cad: { usd: 0.98054, gbp: 1.99169, cad: 1, eur: 1.38814, aud: 0.86613, },
+    eur: { usd: 0.70641, gbp: 1.43448, cad: 0.72037, eur: 1, aud: 0.62382, },
+    aud: { usd: 1.13262, gbp: 2.29964, cad: 1.15498, eur: 1.60329, aud: 1, },
+};
+
+// convert currency function
+function convertCurrency(baseCurrency) {
+    const baseValue = parseFloat(document.getElementById(baseCurrency).value);
+
+    if (isNaN(baseValue)) return;
+
+    for (let currency in conversionRates[baseCurrency]) {
+        if (currency !== baseCurrency) {
+            const convertedValue = (baseValue * conversionRates[baseCurrency][currency]).toFixed(5);
+            document.getElementById(currency).value = convertedValue;
+        };
+    };
+}
+
+
+
+
+
+
+
+
+
+
 window.onload = function() {
     displayFortune(array);
     
